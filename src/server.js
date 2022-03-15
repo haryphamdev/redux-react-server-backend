615f92d97594a46e8103a2f7a982339b412cc741
Hoi Dan IT with Eric <haryphamdev@gmail.com>
2022-03-15 09:28:15 +0700

    init project

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import configViewEngine from "./config/viewEngine";
+import initWebRoutes from "./routes/web";
+require("dotenv").config();
+import bodyParser from 'body-parser';
+// import connection from "./config/connectDB";
+
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+//config view engine
+configViewEngine(app);
+
+//config body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//test connection db
+// connection();
+
+//init web routes
+initWebRoutes(app);
+
+app.listen(PORT, () => {
+    console.log(">>> JWT Backend is running on the port = " + PORT);
+})
