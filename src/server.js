368768dc8445c345ee7d250eee274fdda399cb24
Hoi Dan IT with Eric <haryphamdev@gmail.com>
2022-03-16 09:14:05 +0700

    update cors

--- src/server.js
+++ src/server.js
@@ -9,6 +9,29 @@ import bodyParser from 'body-parser';
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+//config cors
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', process.env.REACT_URL);
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    // Pass to next layer of middleware
+    next();
+});
+
+
 //config view engine
 configViewEngine(app);
 
